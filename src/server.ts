03387d8f25445f5546173aea12dd1ae30ee96ab9
GeekyRavi <rajravikant042@gmail.com>
2024-07-21 20:49:46 +0530

    cors-remove

--- src/server.ts
+++ src/server.ts
@@ -19,9 +19,9 @@ const port = process.env.PORT || 5000 ;
 const app = express();
 
 app.use(morgran("dev"));
-app.use(cors({
-  origin:"*"
-}));
+// app.use(cors({
+//   origin:"*"
+// }));
 app.use(
   express.json({
     limit: "16kb",
